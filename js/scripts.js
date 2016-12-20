f40a1cac4568755621482f91f6da10c3c5713cf8
ea <elliea915@gmail.com>
2016-12-20 15:14:16 -0800

    added fades

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
 
+//fadeIn
+function myFunction() {
+  location.reload();
+}
 
-// 2. This code loads the IFrame Player API code asynchronously.
+//video
  var tag = document.createElement('script');
 
  tag.src = "https://www.youtube.com/iframe_api";
  var firstScriptTag = document.getElementsByTagName('script')[0];
  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
- // 3. This function creates an <iframe> (and YouTube player)
- //    after the API code downloads.
+
  var player;
  function onYouTubeIframeAPIReady() {
    player = new YT.Player('player', {
@@ -23,14 +26,12 @@
    });
  }
 
- // 4. The API will call this function when the video player is ready.
+
  function onPlayerReady(event) {
-   event.target.playVideo();
+   event.target.playVideo(2000);
  }
 
- // 5. The API calls this function when the player's state changes.
- //    The function indicates that when playing a video (state=1),
- //    the player should play for six seconds and then stop.
+
  var done = false;
  function onPlayerStateChange(event) {
    if (event.data == YT.PlayerState.PLAYING && !done) {
